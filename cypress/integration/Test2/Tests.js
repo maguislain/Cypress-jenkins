e2470bbebf4d8fa72734ba5687641d90d13b0664
marc-antoine <maguislain@bitoo.fr>
2021-06-06 12:16:43 +0200

    first commit

---
+++ cypress/integration/Test2/Tests.js
@@ -0,0 +1,12 @@
+import { it } from "mocha"
+
+describe('test', function() {
+
+    it('test1', function(){
+
+        cy.visit('www.amazon.fr')
+
+    })
+
+    
+})
---
+++ cypress/integration/Test3/Tests.js
@@ -0,0 +1,10 @@
+describe('test', function() {
+
+    it('test1', function(){
+
+        cy.visit('/')
+
+    })
+
+    
+})
